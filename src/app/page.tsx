0ee2de8dd9f10db1efa43862a4fbe4ce58cb3ba2
Hypecharge Studio <119539182+HyperchargeStudio@users.noreply.github.com>
2024-02-05 19:18:19 +0530

    Change Structure of Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,20 @@
 "use client";
-import Configuration from "@/components/Configuration";
 import Navigation from "@/components/Navigation";
-import ChunkVisalizer from "@/components/ChunkVisualizer";
-import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
+import ChunkComposer from "@/components/ChunkComposer";
+import ChunkVisualizer from "@/components/ChunkVisualizer";
 
 export default function Home() {
-  const [text, setText] = useState("");
-  const handleTextAreaInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    e.preventDefault();
-    setText(e.currentTarget.value);
-  };
   return (
-    <main className="flex flex-col items-center">
-      <section className="flex flex-col  items-center w-full">
-        <Navigation />
-
-        <section className="flex w-full">
-          <div className="flex justify-center w-full">
-            <div className="w-5/6">
-              <ChunkVisalizer Text={text} />
-            </div>
-          </div>
-          <div className="px-10 bg-stone-600 ">
-            <div className="mb-16 w-full">
-              <h3>Text Area</h3>
-              <Textarea
-                onInput={handleTextAreaInput}
-                value={text}
-                placeholder="Start writing or Ctrl+V to paste text here."
-              />
-            </div>
-            <Configuration />
-          </div>
+    <main className="flex flex-col">
+      <div className="w-auto mx-24 flex flex-col">
+        <section className="px-12 flex flex-col gap-10 bg-white/[0.04] pt-6 rounded-b-3xl">
+          <Navigation />
+          <ChunkComposer />
         </section>
-      </section>
+        <section>
+          <div></div>
+        </section>
+      </div>
     </main>
   );
 }
